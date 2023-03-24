72e1445064d28cd8f673bc6ae80bfa2b5b9e200b
hello05rodelli <127855203+hello05rodelli@users.noreply.github.com>
2023-03-24 12:24:41 -0300

    Add files via upload

---
+++ atv01/index.js
@@ -0,0 +1,13 @@
+const {
+    factorialize
+} = require('./src/array-utils/questao01')
+
+const {palindromo}= require('./src/array-utils/questao02')
+
+
+//Questão 01
+console.log(factorialize(5));
+
+//Questão 02
+
+palindromo("araradidi")
---
+++ atv01/src/array-utils/questao02.js
@@ -0,0 +1,11 @@
+function palindromo (p) {
+var dir = ""
+
+for (var i = p.length-1; i >= 0; i--) {
+    console.log(p[i])
+    
+}
+}
+module.exports={
+    palindromo
+}
